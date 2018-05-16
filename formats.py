3e95e650e488c81cbc70232037606fa79ae97fb1
aguamenti78 <37822406+aguamenti78@users.noreply.github.com>
2018-05-16 12:27:49 -0700

    Update formats.py

--- formats.py
+++ formats.py
@@ -1,5 +1,5 @@
 class vote:
-  text = '**Upvote** if this post has **good rhyme/meter ("True BootTooBig"). Downvote** if this post has **bad rhyme/meter ("Small Boots"). Report** this post if it requires further moderator action, such as being a repost or being offensive. \n\n'
+  text = '**Upvote** if this post has **good rhyme AND meter ("True BootTooBig"). Downvote** if this post has **bad rhyme/meter ("Small Boots"). Report** this post if it requires further moderator action, such as being a repost or being offensive. \n\n'
   notSunday = 'If the score of this comment falls too low, the post will be removed. \n\n'
   sunday = 'This post is posted on Small Boot Sunday \n\n'
 
